834d403d5e8d89e2627efe4a659382c7a0185086
Jontonios <joseantoniorsystem@gmail.com>
2021-08-18 14:19:18 +0200

    pre production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBMmlDoes6AYQ5inG33o6StCM7S4EFUzoQ",
+    authDomain: "chanka-web.firebaseapp.com",
+    projectId: "chanka-web",
+    storageBucket: "chanka-web.appspot.com",
+    messagingSenderId: "559487543536",
+    appId: "1:559487543536:web:d5c98a82dc3af5f2dcfa35",
+    measurementId: "G-TD2PR10DRE"
+  }
 };
